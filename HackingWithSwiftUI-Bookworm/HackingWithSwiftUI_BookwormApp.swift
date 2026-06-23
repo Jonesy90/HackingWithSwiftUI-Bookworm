2dbcfdb9d964c69824df3648c22a1d06896b0158
Michael Jones <michael.jones90@me.com>
2026-06-23 13:10:58 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-Bookworm/HackingWithSwiftUI_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_BookwormApp.swift
+//  HackingWithSwiftUI-Bookworm
+//
+//  Created by Michael Jones on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
